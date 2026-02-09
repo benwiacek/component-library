a0dc1bfb8715f20f4d111687e857f33f9c3f15a2
Wiacek <benjamin.wiacek@gmail.com>
2026-02-09 17:41:06 -0500

    Built basic structure for badges

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,59 @@
-
+import Badge from "./Badge.jsx"
 
 export default function App() {
 
-  return (
-    <>
-      <h1>Test App</h1>
-    </>
-  )
+	return (
+		<main>
+			<h1>React Component Library</h1>
+
+			<h2>Badges</h2>
+			<section>
+				<Badge color="" style="">Test 1</Badge>
+				<Badge color="" style="">Test 2</Badge>
+				<Badge color="" style="">Test 3</Badge>
+				<Badge color="" style="">Test 4</Badge>
+				<Badge color="" style="">Test 5</Badge>
+				<Badge color="" style="">Test 6</Badge>
+				<Badge color="" style="">Test 7</Badge>
+				<Badge color="" style="">Test 8</Badge>
+			</section>
+
+			<section>
+				<Badge color="" style="">Test 1</Badge>
+				<Badge color="" style="">Test 2</Badge>
+				<Badge color="" style="">Test 3</Badge>
+				<Badge color="" style="">Test 4</Badge>
+				<Badge color="" style="">Test 5</Badge>
+				<Badge color="" style="">Test 6</Badge>
+				<Badge color="" style="">Test 7</Badge>
+				<Badge color="" style="">Test 8</Badge>
+			</section>
+
+			<h2>Banners</h2>
+			<section>
+				
+			</section>
+
+			<h2>Cards</h2>
+			<section>
+				
+			</section>
+
+			<h2>Testimonials</h2>
+			<section>
+				
+			</section>
+
+			<h2>Tooltips</h2>
+			<section>
+				
+			</section>
+
+			<h2>Toasts</h2>
+			<section>
+				
+			</section>
+
+		</main>
+	)
 }
---
+++ src/Badge.jsx
@@ -0,0 +1,8 @@
+export default function Badge({ children }) {
+    
+    return (
+        <span className="badge">
+            {children}
+        </span>
+    )
+}
