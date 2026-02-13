6462cbe8295d996d021aa2f3a5c88c8819bb31e3
Ben Wiacek <benjamin.wiacek@gmail.com>
2026-02-12 19:41:12 -0500

    Refactored banners with an object of objects for each variant

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -5,54 +5,46 @@ import { FaExclamationTriangle } from "react-icons/fa"
 import { FaCircleXmark } from "react-icons/fa6"
 import { HiInformationCircle } from "react-icons/hi"
 
-
-
 export default function Banner({ variant, children }) {
 
-    const success = variant === "success"? true : false
-    const warning = variant === "warning"? true : false
-    const error = variant === "error"? true : false
-    const neutral = variant === "neutral"? true : false
-
-    function renderBanner() {
-
-        const headlineClass = clsx("headline", {
-            "head-success": success,
-            "head-warning": warning,
-            "head-error": error,
-            "head-neutral": neutral
-        })
-
-        const iconClass = clsx("icon", {
-            "green-mark": success,
-            "warning-tri": warning,
-            "red-cross": error,
-            "blue-icon": neutral
-        })
-
-            return (
-                <>
-                    {success && <FaCheckCircle className={iconClass}/>}
-                    {warning && <FaExclamationTriangle className={iconClass}/>}
-                    {error && <FaCircleXmark className={iconClass}/>}
-                    {neutral && <HiInformationCircle className={iconClass}/>}
-                    
-                    <div>
-                        <span className={headlineClass}>
-                            {success && "Congratulations!"}
-                            {warning && "Attention"}
-                            {error && "There is a problem with your application"}
-                            {neutral && "Update available"}
-                        </span>
-                        {children && <p>{children}</p>}
-                    </div>
-                </>
-            )
+    const variants = {
+        success: {
+            icon: FaCheckCircle,
+            iconClass: "icon green-mark",
+            headline: "Congratulations!",
+            headlineClass: "headline head-success",
+        },
+        warning: {
+            icon: FaExclamationTriangle,
+            iconClass: "icon warning-tri",
+            headline: "Attention",
+            headlineClass: "headline head-warning",
+        },
+        error: {
+            icon: FaCircleXmark,
+            iconClass: "icon red-cross",
+            headline: "There is a problem with your application",
+            headlineClass: "headline head-error",
+        },
+        neutral: {
+            icon: HiInformationCircle,
+            iconClass: "icon blue=icon",
+            headline: "Update available",
+            headlineClass: "headline head-neutral",
+        }
     }
+
+    const selected = variants[variant]
 
     return (
         <div className={classnames(variant, "banner")}>
-            {renderBanner()}
+            <selected.icon className={selected.iconClass}/>
+            <div>
+                <span className={selected.headlineClass}>
+                    {selected.headline}
+                </span>
+                {children && <p>{children}</p>}
+            </div>
         </div>
     )
 }
